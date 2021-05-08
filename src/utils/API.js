14d9115cf5509c10b87a51dc0037a9aa0ad0a97a
Karen <karenyaa@live.co.uk>
2021-05-08 16:23:31 +0100

    created API call to get randomUsers

---
+++ src/utils/API.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+const API = {
+    getUsers: function () {
+        return axios.get("https://randomuser.me/api/?results=100");
+    }
+
+};
+
+export default API; 
