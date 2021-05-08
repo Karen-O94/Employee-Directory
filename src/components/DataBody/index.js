d77fec14b7868edcccd0aee7f39e0f3e591d376b
Karen <karenyaa@live.co.uk>
2021-05-08 17:47:52 +0100

    created function to format date

--- src/components/DataBody/index.js
+++ src/components/DataBody/index.js
@@ -1,3 +1,18 @@
 import React, { useContext } from "react";
 import "./style.css"
 import DataAreaContext from "../../utils/DataAreaContext"
+
+const DataBody = () => {
+    const context = useContext(DataAreaContext);
+
+    function formatDate(date){
+        const dateArray = date.split("-");
+        const year = dateArray[0];
+        const month = dateArray[1];
+        const dayArray = dateArray[2].split("T");
+        const day = dayArray[0];
+        const formattedDate =[month, day, year].join("-");
+        return formattedDate;
+    }
+
+}
