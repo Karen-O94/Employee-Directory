485797f8f8266aabc590510e5f5a7957dd69f806
Karen <karenyaa@live.co.uk>
2021-05-08 16:24:15 +0100

    amended end point for API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
+// Export an object containing methods we'll use for accessing the RandomUser API
 
 const API = {
     getUsers: function () {
-        return axios.get("https://randomuser.me/api/?results=100");
+        return axios.get("https://randomuser.me/api/?results=100&nat=gb");
     }
 
 };
