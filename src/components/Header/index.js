30eb318eee31a0ead863eb2c12304e2d49f958ad
Karen <karenyaa@live.co.uk>
2021-05-08 15:58:46 +0100

    added components folder and a folder for the Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => (
+  <div className="header">
+    <h1>Employee Directory</h1>
+    <p>
+      Please see employees below
+    </p>
+  </div>
+);
+
+export default Header;
