18b822bba4b2838fe3a2e792c00c8dd84a204d18
Karen <karenyaa@live.co.uk>
2021-05-08 16:58:34 +0100

    setting state for DataArea with usestate method

--- src/components/DataArea/index.js
+++ src/components/DataArea/index.js
@@ -3,3 +3,18 @@ import API from "../../utils/API";
 import DataAreaContext from "../../utils/DataAreaContext";
 import './style.css';
 
+const DataArea = () => {
+    const [developerState, setDeveloperState] = useState({
+      users: [],
+      order: "ascend",
+      filteredUsers: [],
+      headings: [
+        { name: "Image", width: "10%", },
+        { name: "Name", width: "10%", },
+        { name: "Phone", width: "20%", },
+        { name: "Email", width: "20%", },
+        { name: "DOB", width: "10%", }
+      ]
+    });
+
+}
