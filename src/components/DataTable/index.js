98210821805f72775bdafc359e522d644a6ac502
Karen <karenyaa@live.co.uk>
2021-05-08 17:34:24 +0100

    returning div to store information in columns

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -2,3 +2,43 @@ import React, { useContext } from "react";
 import DataBody from "../DataBody";
 import "./DataTable.css";
 import DataAreaContext from "../../utils/DataAreaContext";
+
+const DataTable = () => {
+    //storing context as a variable
+    const context = useContext(DataAreaContext);
+
+    return (
+
+        <div className="datatable mt-5">
+            <table
+                id="table"
+                className="table table-striped table-hover table-condensed"
+            >
+                <thead>
+                    <tr>
+                        {/* loops through headings and returns the name and width */}
+                        {context.developerState.headings.map(({ name, width }) => {
+                            return (
+                                <th
+                                    className="col"
+                                    key={name}
+                                    style={{ width }}
+                                    onClick={() => {
+                                        context.sortingItems(name.toLowerCase());
+                                    }}
+                                >
+                                    {name}
+                                    <span className="pointer"></span>
+                                </th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+
+                <DataBody />
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
